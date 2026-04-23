78ce198119895f39670cbe4761a7a7f5a1726150
TTRRYYUR <tchainikov0702@yandex.ru>
2026-04-23 16:34:01 +0300

    3 задание.py

--- 3 задание.py
+++ 3 задание.py
@@ -1,21 +1,26 @@
-def input():
+def input_list():
     n = int(input())
-    pass
-    return number
+    numbers = []
 
-def search_rate_number(number):
+    
+    while len(numbers) < n:
+        line_numbers = list(map(int, input().split()))
+        for num in line_numbers:
+            numbers.append(num)
+    return numbers
+
+
+def search_rate_number(numbers):
     rate = {}
     for num in number:
-        if num in rate:
-            rate[num] += 1
-        else:
-            rate[num] = 1
+        rate[num] = rate.get(num, 0) + 1
     return rate
 
 
 def search_min_rate(rate):
     minimum_rate = min(rate.values())
-    return minimum_rate 
+    return minimum_rate
+
 
 def search_max_number(rate, min_rate_number):
     max_number = []
@@ -25,7 +30,7 @@ def search_max_number(rate, min_rate_number):
     return max(max_number)
 
 
-arr = input()
+arr = input_list()
 rate = search_rate_number(arr)
 min_rate = search_min_rate(rate)
 max_number = search_max_number(rate, min_rate)
